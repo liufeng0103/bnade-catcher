8269d887c3ce9f06eabf84d303002ef74857e681
Feng Liu <liufeng0103@163.com>
2017-07-13 23:51:55 +0800

    添加4中暗月套牌不更新bonus

--- src/main/java/com/bnade/wow/catcher/ItemCatcherJob.java
+++ src/main/java/com/bnade/wow/catcher/ItemCatcherJob.java
@@ -45,7 +45,7 @@ public class ItemCatcherJob implements Job {
     /**
      * 添加新物品到item表
      */
-     public void addNewItems() {
+    public void addNewItems() {
         logger.info("开始更新新物品");
         try {
             List<Integer> itemIds = itemDao.getIds();
@@ -98,7 +98,7 @@ public class ItemCatcherJob implements Job {
      * 3. 使用一个list保存那些确定不需要bonus
      * 4. 注意有空bonus的添加
      */
-     public void addNewItemBonuses() {
+    public void addNewItemBonuses() {
         logger.info("开始更新物品bonus list");
         // 不需要bonus的物品
         Set<Integer> ignoredItemIds = new HashSet<>();
@@ -108,6 +108,9 @@ public class ItemCatcherJob implements Job {
         ignoredItemIds.add(146669); // 哨兵的永恒庇护所 - 护甲(class=2)
         ignoredItemIds.add(146666); // 塞露布拉，暗夜的双子 - 护甲(class=2)
         ignoredItemIds.add(128709); // 暗月套牌：地狱火 - 护甲(class=2)
+        ignoredItemIds.add(128705); // 暗月套牌：统御
+        ignoredItemIds.add(128711); // 暗月套牌：不朽
+        ignoredItemIds.add(128710); // 暗月套牌：应许
 
         try {
             List<ItemBonus> itemBonuses = itemDao.findAllItemBonuses();
