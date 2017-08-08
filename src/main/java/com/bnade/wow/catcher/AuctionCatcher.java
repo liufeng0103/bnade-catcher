fbab79f82e650085e62f48f449dc37f7824040b9
Feng Liu <liufeng0103@163.com>
2017-08-08 11:37:47 +0800

    保存所有拍卖数据， 有些第三方应用还在使用

--- src/main/java/com/bnade/wow/catcher/AuctionCatcher.java
+++ src/main/java/com/bnade/wow/catcher/AuctionCatcher.java
@@ -138,11 +138,12 @@ public class AuctionCatcher {
 
 		long start;
 		// 保存拍卖数据
+		start = System.currentTimeMillis();
 		AuctionDao auctionDao = DaoFactory.getAuctionDao();
-//		logger.info("[{}]删除上一次拍卖行数据", realm.getName());
-//		auctionDao.deleteAll(realm.getId());
-//		auctionDao.insert(realm.getId(), aucs);
-//		logger.info("[{}]保存{}条拍卖行数据完毕, 用时{}", realm.getName(), aucs.size(), TimeUtils.format(System.currentTimeMillis() - start));
+		logger.info("[{}]删除上一次拍卖行数据", realm.getName());
+		auctionDao.deleteAll(realm.getId());
+		auctionDao.insert(realm.getId(), aucs);
+		logger.info("[{}]保存{}条拍卖行数据完毕, 用时{}", realm.getName(), aucs.size(), TimeUtils.format(System.currentTimeMillis() - start));
 //
 		// 保存所有最低一口价数据
 		logger.info("[{}]删除拍卖行最低一口价数据", realm.getName());
